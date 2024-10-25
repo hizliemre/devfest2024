7cbef0814b028095b8b57777f2964a0929fd0485
Emre Hızlı <hizliemre@gmail.com>
2024-10-25 20:46:36 +0300

    Initial commit

---
+++ src/pages/create-todo/create-todo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-todo',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-todo.component.html',
+  styleUrl: './create-todo.component.scss'
+})
+export class CreateTodoComponent {
+
+}
---
+++ src/state/todos.state.ts
@@ -0,0 +1,39 @@
+import {computed, inject, Injectable, signal} from '@angular/core';
+import {TodoApi} from '@data-access/todo.api';
+import {rxResource} from '@angular/core/rxjs-interop';
+import {TodoStatusChangedEvent} from '@components/todo/todo.component';
+import {take} from 'rxjs';
+
+@Injectable()
+export class TodosState {
+
+  #todoApi = inject(TodoApi);
+
+  #limit = signal(30);
+  #skip = signal(0);
+  #todosResource = rxResource({
+    request: () => ({limit: this.#limit(), skip: this.#skip()}),
+    loader: ({request: params}) => this.#todoApi.getTodos(params.limit, params.skip),
+  })
+
+  todos = computed(() => this.#todosResource.value() ?? []);
+  loading = computed(() => this.#todosResource.isLoading());
+
+  prev() {
+    if (this.#skip() === 0) return;
+    this.#skip.set(this.#skip() - this.#limit());
+  }
+
+  next() {
+    this.#skip.set(this.#skip() + this.#limit());
+  }
+
+  itemStatusChanged(e: TodoStatusChangedEvent) {
+    this.#todoApi.updateStatus(e.id, e.status)
+      .pipe(take(1))
+      .subscribe(() => {
+        this.#todosResource.reload();
+      });
+  }
+
+}
