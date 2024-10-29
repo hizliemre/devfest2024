344640855ade21c5bb18a5367afa6315e35e9dcc
Emre Hızlı <hizliemre@gmail.com>
2024-10-29 15:08:21 +0300

    clean fire

--- src/pages/login/login.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    standalone: true,
-    selector: 'app-login',
-    templateUrl: 'login.component.html',
-})
-export class LoginComponent {}
