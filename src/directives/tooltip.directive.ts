488192845c2e0a37930f79af582129858e8d4683
Emre Hızlı <hizliemre@gmail.com>
2024-10-29 14:49:30 +0300

    prettier config file

--- src/directives/tooltip.directive.ts
+++ src/directives/tooltip.directive.ts
@@ -1,70 +1,53 @@
-import {
-  Component,
-  ComponentRef,
-  Directive,
-  inject,
-  input,
-  OnDestroy,
-  Renderer2,
-  ViewContainerRef,
-} from '@angular/core';
+import { Component, ComponentRef, Directive, inject, input, OnDestroy, Renderer2, ViewContainerRef } from '@angular/core';
 
 @Component({
-  selector: 'app-tooltip',
-  standalone: true,
-  template: `{{ text() }}`,
-  host: {
-    class: 'absolute pointer-events-none bg-black text-white p-2 rounded-lg',
-  },
+    selector: 'app-tooltip',
+    standalone: true,
+    template: `{{ text() }}`,
+    host: {
+        class: 'absolute pointer-events-none bg-black text-white p-2 rounded-lg',
+    },
 })
 class TooltipComponent {
-  text = input.required<string>();
+    text = input.required<string>();
 }
 
 @Directive({
-  selector: '[tooltip]',
-  standalone: true,
-  host: {
-    '(mouseenter)': 'onMouseEnter($event)',
-    '(mouseleave)': 'onMouseLeave()',
-    '(mousemove)': 'onMouseMove($event)',
-  },
+    selector: '[tooltip]',
+    standalone: true,
+    host: {
+        '(mouseenter)': 'onMouseEnter($event)',
+        '(mouseleave)': 'onMouseLeave()',
+        '(mousemove)': 'onMouseMove($event)',
+    },
 })
 export class TooltipDirective implements OnDestroy {
-  tooltip = input.required<string>();
+    tooltip = input.required<string>();
 
-  #vcr = inject(ViewContainerRef);
-  #ref!: ComponentRef<TooltipComponent>;
-  #renderer = inject(Renderer2);
+    #vcr = inject(ViewContainerRef);
+    #ref!: ComponentRef<TooltipComponent>;
+    #renderer = inject(Renderer2);
 
-  onMouseEnter(e: MouseEvent): void {
-    this.#ref = this.#vcr.createComponent(TooltipComponent);
-    this.#ref.setInput('text', this.tooltip());
-    this.#setPosition(e);
-  }
+    onMouseEnter(e: MouseEvent): void {
+        this.#ref = this.#vcr.createComponent(TooltipComponent);
+        this.#ref.setInput('text', this.tooltip());
+        this.#setPosition(e);
+    }
 
-  onMouseLeave(): void {
-    this.#vcr.clear();
-  }
+    onMouseLeave(): void {
+        this.#vcr.clear();
+    }
 
-  onMouseMove(e: MouseEvent): void {
-    this.#setPosition(e);
-  }
+    onMouseMove(e: MouseEvent): void {
+        this.#setPosition(e);
+    }
 
-  ngOnDestroy(): void {
-    this.#vcr.clear();
-  }
+    ngOnDestroy(): void {
+        this.#vcr.clear();
+    }
 
-  #setPosition(e: MouseEvent) {
-    this.#renderer.setStyle(
-      this.#ref.location.nativeElement,
-      'top',
-      `${e.clientY}px`,
-    );
-    this.#renderer.setStyle(
-      this.#ref.location.nativeElement,
-      'left',
-      `${e.clientX + 16}px`,
-    );
-  }
+    #setPosition(e: MouseEvent) {
+        this.#renderer.setStyle(this.#ref.location.nativeElement, 'top', `${e.clientY}px`);
+        this.#renderer.setStyle(this.#ref.location.nativeElement, 'left', `${e.clientX + 16}px`);
+    }
 }
