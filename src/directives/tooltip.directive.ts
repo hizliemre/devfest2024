a5ee98d0e23dda38a5591a7623fcf2e928709f87
Emre Hızlı <hizliemre@gmail.com>
2024-10-30 18:23:15 +0300

    improve the tooltip directive

--- src/directives/tooltip.directive.ts
+++ src/directives/tooltip.directive.ts
@@ -12,7 +12,7 @@ export type TooltipPosition = {
     standalone: true,
     template: `{{ text() }}`,
     host: {
-        class: 'absolute pointer-events-none bg-black text-white p-2 rounded-lg',
+        class: 'absolute pointer-events-none bg-black text-white p-20 rounded-lg',
     },
 })
 class TooltipComponent {
@@ -33,62 +33,70 @@ export class TooltipDirective implements OnDestroy {
     delay = input<number>(150);
     position = input<TooltipPosition>({ x: 'right', y: 'bottom' });
     #vcr = inject(ViewContainerRef);
-    #ref!: ComponentRef<TooltipComponent>;
     #renderer = inject(Renderer2);
     #document = inject(DOCUMENT);
-    #delayRef!: Timeout;
     #offset = 16;
+    #ref!: ComponentRef<TooltipComponent>;
+    #delayRef!: Timeout;
+    #mouseEvent!: MouseEvent;
 
     onMouseEnter(e: MouseEvent): void {
+        this.#mouseEvent = e;
         this.#delayRef = setTimeout(() => {
             this.#ref = this.#vcr.createComponent(TooltipComponent);
             this.#ref.setInput('text', this.tooltip());
             this.#ref.changeDetectorRef.detectChanges();
-            this.#setPosition(e);
+            this.#setPosition();
             clearTimeout(this.#delayRef);
         }, this.delay());
     }
 
     onMouseLeave(): void {
-        this.#vcr.clear();
-        if (this.#delayRef) clearTimeout(this.#delayRef);
+        this.#clear();
     }
 
     onMouseMove(e: MouseEvent): void {
-        this.#setPosition(e);
+        this.#mouseEvent = e;
+        this.#setPosition();
     }
 
     ngOnDestroy(): void {
-        this.#vcr.clear();
-        if (this.#delayRef) clearTimeout(this.#delayRef);
+        this.#clear();
+        this.#document.removeEventListener('mousemove', () => {});
     }
 
-    #setPosition(e: MouseEvent) {
+    #setPosition() {
         if (this.#ref) {
-            const x = this.#calculatePosition(e, 'X');
-            const y = this.#calculatePosition(e, 'Y');
+            const x = this.#calculatePosition('X');
+            const y = this.#calculatePosition('Y');
             this.#renderer.setStyle(this.#ref.location.nativeElement, 'left', `${x}px`);
             this.#renderer.setStyle(this.#ref.location.nativeElement, 'top', `${y}px`);
         }
     }
 
-    #calculatePosition(e: MouseEvent, position: 'X' | 'Y'): number {
+    #calculatePosition(position: 'X' | 'Y'): number {
+        const renderOffset = position === 'Y' ? 0 : this.#offset;
         const componentWidth = this.#ref.location.nativeElement.offsetWidth + this.#offset;
-        const componentHeight = this.#ref.location.nativeElement.offsetHeight + this.#offset;
+        const componentHeight = this.#ref.location.nativeElement.offsetHeight + renderOffset;
         const windowWidth = this.#document.documentElement.clientWidth;
         const windowHeight = this.#document.documentElement.clientHeight;
 
         const componentPos = position === 'Y' ? componentHeight : componentWidth;
         const windowPos = position === 'Y' ? windowHeight : windowWidth;
         const checkPoint = position === 'Y' ? this.position().y === 'bottom' : this.position().x === 'right';
-        const clientPos = e[`client${position}`];
+        const clientPos = this.#mouseEvent[`client${position}`];
 
         return checkPoint
             ? clientPos + componentPos > windowPos
                 ? clientPos - componentPos
-                : clientPos + this.#offset
+                : clientPos + renderOffset
             : clientPos - componentPos < 0
-              ? clientPos + this.#offset
+              ? clientPos + renderOffset
               : clientPos - componentPos;
+    }
+
+    #clear(): void {
+        this.#vcr.clear();
+        if (this.#delayRef) clearTimeout(this.#delayRef);
     }
 }
